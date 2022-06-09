d8dc6b7806432db930ab4fb924e3670049f1d840
Miguel Canessa <canessaalvamiguel@gmail.com>
2022-06-08 21:31:28 -0500

    Code without Interface Segregation

---
+++ src/InterfaceSegregation.java
@@ -0,0 +1,52 @@
+interface IWorker{
+    void teaBreak();
+    void recharge();
+    void lunch();
+    void oilCheck();
+}
+
+class HumanWorker implements IWorker{
+
+    @Override
+    public void teaBreak() {
+
+    }
+
+    @Override
+    public void recharge() {
+
+    }
+
+    @Override
+    public void lunch() {
+
+    }
+
+    @Override
+    public void oilCheck() {
+
+    }
+}
+
+class RobotWorker implements IWorker{
+
+    @Override
+    public void teaBreak() {
+
+    }
+
+    @Override
+    public void recharge() {
+
+    }
+
+    @Override
+    public void lunch() {
+
+    }
+
+    @Override
+    public void oilCheck() {
+
+    }
+}
